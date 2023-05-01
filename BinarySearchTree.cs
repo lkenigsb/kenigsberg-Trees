3507a43d1370bbb513b318832b5b0fcc1d8746f7
Ken Solkowitz <libbykenigsberg@gmail.com>
2023-05-01 16:28:39 -0400

    initial commit

---
+++ BinarySearchTree.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace AVL_Trees_PA3
+{
+
+    // BinarySearchTree class
+    //
+    // CONSTRUCTION: with no initializer
+    //
+    // ***************************************************************
+    // void insert( x ) --> Insert x
+    // void remove( x ) --> Remove x
+    // boolean contains( x ) --> Return true if x is present
+    // Comparable findMin( ) --> Return smallest item
+    // Comparable findMax( ) --> Return largest item
+    // boolean isEmpty( ) --> Return true if empty; else false
+    // void makeEmpty( ) --> Remove all items
+    // void printTree( ) --> Print tree in sorted order
+    // String toString() --> Print Tree in a visual fashion
+    // ****************************************************************
+    // Throws UnderflowException as appropriate
+    /**
+    * Implements an unbalanced binary search tree.
+    * Note that all "matching" is based on the compareTo method.
+    * @author Mark Allen Weiss
+    * modified slightly by AK:
+    * added toString()
+    * removed reliance on UnderflowException
+    * rearranged the order of methods
+    */
+    public class BinarySearchTree<AnyType : Comparable<? super AnyType>>
+    {
+        // Basic node stored in unbalanced binary search trees
+
+        private static class BinaryNode<AnyType>
+        {
+            // Constructors
+            BinaryNode(AnyType theElement)
+            {
+                this(theElement, null, null);
+            }
+            BinaryNode(AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt)
+            {
+                element = theElement;
+                left = lt;
+                right = rt;
+            }
+            AnyType element; // The data in the node
+            BinaryNode<AnyType> left; // Left child
+            BinaryNode<AnyType> right; // Right child
+        }
+
+        /** The tree root. */
+        private BinaryNode<AnyType> root;
+        /**
+        * Construct the tree.
+    */
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+
+        /**
+        * Insert into the tree; duplicates are ignored.
+        * @param x the item to insert.
+        */
+        public void insert(AnyType x)
+        {
+            root = insert(x, root);
+        }
+
+        /**
+        * Remove from the tree. Nothing is done if x is not found.
+        * @param x the item to remove.
+        */
+        public void remove(AnyType x)
+        {
+            root = remove(x, root);
+        }
+
+        /**
+        * Find the smallest item in the tree.
+        * @return smallest item or null if empty.
+        */
+        public AnyType findMin()
+        {
+            AnyType found = null;
+            if (!isEmpty())
+            {
+                found = findMin(root).element;
+            }
+            return found;
+        }
+
+        /**
+        * Find the largest item in the tree.
+        *
+        * @return the largest item or null if empty.
+        */
+        public AnyType findMax()
+        {
+            AnyType found = null;
+            if (!isEmpty())
+            {
+                found = findMax(root).element;
+            }
+            return found;
+        }
+
+        /**
+        * Find an item in the tree.
+        * @param x the item to search for.
+        * @return true if not found.
+        */
+        public Boolean contains(AnyType x)
+        {
+            return contains(x, root);
+        }
+
+        /**
+        * Make the tree logically empty.
+        */
+        public void makeEmpty()
+        {
+            root = null;
+        }
+
+        /**
+        * Test if the tree is logically empty.
+        * @return true if empty, false otherwise.
+        */
+        public Boolean isEmpty()
+        {
+            return root == null;
+        }
+
+        /**
+        * Print the tree contents in sorted order.
+        */
+        public void printTree()
+        {
+            if (isEmpty())
+                Console.WriteLine("Empty tree");
+            else
+                printTree(root);
+        }
+
+        /**
+        * return a string with a visual representation of BST
+        * @return String representation of the tree
+        */
+        @Override
+        public String toString()
+        {
+            StringBuilder buffer = new StringBuilder(50);
+            toStr(root, buffer, "", "");
+            return buffer.toString();
+        }
+        /*----------------------------------------------------------
+        *
+        * Private Methods
+        *
+        ----------------------------------------------------------*/
+        /**
+        * Internal method to insert into a subtree.
+        * @param x the item to insert.
+        * @param t the node that roots the subtree.
+        * @return the new root of the subtree.
+    */
+        private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t)
+        {
+            if (t == null)
+                return new BinaryNode<>(x, null, null);
+            int compareResult = x.compareTo(t.element);
+            if (compareResult < 0)
+                t.left = insert(x, t.left);
+            else if (compareResult > 0)
+                t.right = insert(x, t.right);
+            else
+                ; // Duplicate; do nothing. Explicitly emphasized
+            return t;
+        }
+
+        /**
+        * Internal method to remove from a subtree.
+        * @param x the item to remove.
+        * @param t the node that roots the subtree.
+        * @return the new root of the subtree.
+    */
+        private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t)
+        {
+            if (t == null)
+                return t; // Item not found; do nothing
+            int compareResult = x.compareTo(t.element);
+            if (compareResult < 0)
+                t.left = remove(x, t.left);
+            else if (compareResult > 0)
+                t.right = remove(x, t.right);
+            else if (t.left != null && t.right != null) // Two children
+            {
+                t.element = findMin(t.right).element;
+                t.right = remove(t.element, t.right);
+            }
+            else
+                t = (t.left != null) ? t.left : t.right;
+            return t;
+        }
+
+        /**
+        * Internal method to find the smallest item in a subtree.
+        * @param t the node that roots the subtree.
+        * @return node containing the smallest item.
+        */
+        private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t)
+        {
+            if (t == null)
+                return null;
+            else if (t.left == null)
+                return t;
+            return findMin(t.left);
+        }
+
+        /**
+        * Internal method to find the largest item in a subtree.
+        * @param t the node that roots the subtree.
+        * @return node containing the largest item.
+        */
+        private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t)
+        {
+            if (t != null)
+                while (t.right != null)
+                    t = t.right;
+            return t;
+        }
+
+        /**
+        * Internal method to find an item in a subtree.
+        * @param x is item to search for.
+        * @param t the node that roots the subtree.
+        * @return node containing the matched item.
+        */
+        private Boolean contains(AnyType x, BinaryNode<AnyType> t)
+        {
+            if (t == null)
+                return false;
+            int compareResult = x.compareTo(t.element);
+            if (compareResult < 0)
+                return contains(x, t.left);
+            else if (compareResult > 0)
+                return contains(x, t.right);
+            else
+                return true; // Match
+        }
+
+        /**
+        * Internal method to print a subtree in sorted order.
+        * @param t the node that roots the subtree.
+        */
+        private void printTree(BinaryNode<AnyType> t)
+        {
+            if (t != null)
+            {
+                printTree(t.left);
+                Console.WriteLine(t.element);
+                printTree(t.right);
+            }
+        }
+
+        /** Internal method to prepare a string display
+        * of a BST in a manner of DOS-file structure
+        * @param node starting node
+        * @param buffer - dynamically generated connections
+        * @param prefix - connection prefix for the node
+        * @param childrenPrefix - connection prefix for the children
+        */
+        private void toStr(BinaryNode<AnyType> node,
+        StringBuilder buffer,
+        String prefix,
+        String childrenPrefix)
+        {
+            buffer.Append(prefix);
+            buffer.Append(node.element.toString());
+            buffer.Append('\n');
+            if (node.right != null)
+            {
+                toStr(node.right, buffer, childrenPrefix + ">── ",
+                node.left != null
+                ? childrenPrefix + "│ "
+                : childrenPrefix + " ");
+            }
+            if (node.left != null)
+            {
+                toStr(node.left, buffer, childrenPrefix + "<── ", childrenPrefix + " ");
+            }
+        }
+
+        /**
+        * Internal method to compute height of a subtree.
+        * @param t the node that roots the subtree.
+    */
+        // private int height( BinaryNode<AnyType> t )
+        // {
+        // if( t == null )
+        // return -1;
+        // else
+        // return 1 + Math.max( height( t.left ), height( t.right ) );
+        // }
+        //
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,274 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+/**
+ * 
+ * TO DO:
+ * 1. time the insertion method
+ * 2. time the contains method
+ * 3. time the remove method
+ *  
+ * Refer to https://www.geeksforgeeks.org/how-to-calculate-the-code-execution-time-in-c-sharp/ to
+ * time execution.
+ */
+
+namespace AVL_Trees_PA3
+{
+    public class Program
+    {
+
+        /**
+         * Your ****insertion**** test should time inserting 1,000,000 numbers from 0 to 999,999 in random 
+         * order into a tree. 
+         * Make sure that there are no repetitions and that all the numbers are, in fact, inserted.
+         */
+
+        Stopwatch AVLInsertWatch;
+        Stopwatch BSTInsertionWatch;
+        Stopwatch SplayInsertWatch;
+
+        Stopwatch AVLContainsWatch;
+        Stopwatch BSTContainsWatch;
+        Stopwatch SplayContainsWatch;
+
+        Stopwatch AVLRemovalWatch;
+        Stopwatch BSTRemovalWatch;
+        Stopwatch SplayRemovalWatch;
+        public void InsertionAVL()
+        {
+            //begins calculating the execution time
+            AVLInsertWatch = Stopwatch.StartNew();
+
+            //Create AVL tree
+
+            for (int i = 0; i < 999999; i++)
+            {
+                //treeName.insert(i)
+            }
+
+            Boolean allThere = true;
+            for(int i = 0; i < 999999; i++)
+            {
+                //check that all numbers were inserted - use contains method
+                if(//treeName.contains(i) == false)
+                {
+                    allThere = false;
+                    Console.WriteLine("Not all numbers are in the AVL Tree correctly");
+                    break;
+                }
+            }
+
+            AVLInsertWatch.Stop(); 
+        }
+
+        public void InsertionBST()
+        {
+
+            //begins calculating the execution time
+            BSTInsertionWatch = Stopwatch.StartNew();
+
+            //Create BST tree
+
+
+            for (int i = 0; i < 999999; i++)
+            {
+                //treeName.insert(i)
+            }
+
+            Boolean allThere = true;
+            for (int i = 0; i < 999999; i++)
+            {
+                //check that all numbers were inserted - use contains method
+                if (//treeName.contains(i) == false)
+                {
+                    allThere = false;
+                    Console.WriteLine("Not all numbers are in the BST Tree correctly");
+                    break;
+                }
+            }
+
+            BSTInsertionWatch.Stop();
+        }
+
+        public void InsertionSplay()
+        {
+            //begins calculating the execution time
+            SplayInsertWatch = Stopwatch.StartNew();
+
+            //Create Splay tree
+
+
+            for (int i = 0; i < 999999; i++)
+            {
+                //treeName.insert(i)
+            }
+
+            Boolean allThere = true;
+            for (int i = 0; i < 999999; i++)
+            {
+                //check that all numbers were inserted - use contains method
+                if (//treeName.contains(i) == false)
+                {
+                    allThere = false;
+                    Console.WriteLine("Not all numbers are in the Splay Tree correctly");
+                    break;
+                }
+            }
+
+            SplayInsertWatch.Stop();
+        }
+        /**
+        ****Containment**** test should time testing contains method ten times for each number from 0 to 999,999 in
+         * the tree created above: 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, ..., 2, 2, 2, etc. 
+         * If any number is not found, an error should be printed.
+         */
+        public void ContainsAVL()
+        {
+            //Start watch
+            AVLContainsWatch = Stopwatch.StartNew();
+
+            //create AVL Tree
+            //insert numbers 0-9999999
+
+            for(int i = 0; i < 9999999; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    //if(treeName.contains(i) == false)
+                    {
+                        Console.WriteLine("Error, number not found");
+                        break;
+                    }
+                }
+            }
+
+            AVLContainsWatch.Stop();
+        }
+
+        public void ContainsBST()
+        {
+            //Start watch
+            BSTContainsWatch = Stopwatch.StartNew();
+
+            //create BST Tree
+            //insert numbers 0-9999999
+
+            for (int i = 0; i < 9999999; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    //if(treeName.contains(i) == false)
+                    {
+                        Console.WriteLine("Error, number not found");
+                        break;
+                    }
+                }
+            }
+
+            BSTContainsWatch.Stop();
+        }
+
+        public void ContainsSplay()
+        {
+            //Start watch
+            SplayContainsWatch = Stopwatch.StartNew();
+
+            //create Splay Tree
+            //insert numbers 0-9999999
+
+            for (int i = 0; i < 9999999; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    //if(treeName.contains(i) == false)
+                    {
+                        Console.WriteLine("Error, number not found");
+                        break;
+                    }
+                }
+            }
+
+            SplayContainsWatch.Stop();
+        }
+
+        /**
+         * ****Removal**** test should time remove method for all numbers 0 to 999,999 in that order
+         */
+        public void AVLRemoval()
+        {
+            //start watch
+            AVLRemovalWatch = Stopwatch.StartNew();
+
+            //create AVL tree
+            //Insert numers 0-999999
+
+            for(int i = 0; i < 999999; i++)
+            {
+                //AVLTree.remove(i)
+            }
+
+            AVLRemovalWatch.Stop();
+        }
+
+        public void BSTRemoval()
+        {
+            //start watch
+            BSTRemovalWatch = Stopwatch.StartNew();
+
+            //create BST tree
+            //Insert numers 0-999999
+
+            for (int i = 0; i < 999999; i++)
+            {
+                //AVLTree.remove(i)
+            }
+
+            BSTRemovalWatch.Stop();
+        }
+
+        public void SplayRemoval()
+        {
+            //start watch
+            SplayRemovalWatch = Stopwatch.StartNew();
+
+            //create Splay tree
+            //Insert numers 0-999999
+
+            for (int i = 0; i < 999999; i++)
+            {
+                //AVLTree.remove(i)
+            }
+
+            SplayRemovalWatch.Stop();
+        }
+
+        static public void Main(String[] args)
+        {
+            Program program = new Program();
+
+            program.InsertionAVL();
+            program.InsertionBST();
+            program.InsertionSplay();
+
+            program.ContainsAVL();
+            program.ContainsBST();
+            program.ContainsSplay();
+
+            program.AVLRemoval();
+            program.BSTRemoval();
+            program.SplayRemoval();
+
+            Console.WriteLine("Test:\t\tInsert:\t|\tContains\t|\tRemove\t|");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine($"AVL:\t|\t{program.AVLInsertWatch}\t|\t{program.AVLContainsWatch}\t|\t{program.AVLRemovalWatch}\t|");
+            Console.WriteLine($"BST:\t|\t{program.BSTInsertionWatch}\t|\t{program.BSTContainsWatch}\t|\t{program.BSTRemovalWatch}\t|");
+            Console.WriteLine($"Splay:\t|\t{program.SplayInsertWatch}\t|\t{program.SplayContainsWatch}\t|\t{program.SplayRemovalWatch}\t|");
+
+
+        }
+
+    }
+}
